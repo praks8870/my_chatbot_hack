47e5563179a5d3bf534d579fbfbb66244eb7fd4d
praks8870 <praks8870@gmail.com>
2024-11-23 09:52:33 +0530

    Update app2.py

--- app2.py
+++ app2.py
@@ -11,7 +11,7 @@ import torch.optim as optim
 from flask_session import Session
 
 app = Flask(__name__)
-
+nltk.download('punkt')
 # Configure session for chat history
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
